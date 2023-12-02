18b5c6ebb60d578929338015abbd88395a5157d1
Trey Wood <treyw@squareup.com>
2023-12-01 23:19:34 -0700

    day 1

--- day-1/Main.hs
+++ day-1/Main.hs
@@ -2,13 +2,27 @@ module Main where
 
 import Aoc
 
-data Input = Input Int Int Int deriving (Show)
-
 main :: IO ()
 main = run parser parts
   where
-    parser = Input <$> nextInt <*> nextInt <*> nextInt
-    nextInt = integer <* eol
+    parser = sepEndBy (til int *> some (int <* til (int <|> newline))) newline
+    til p = manyTill letterChar (try $ lookAhead p)
+    int =
+        choice
+            [ digitChar
+            , '1' <$ stringRewind "one"
+            , '2' <$ stringRewind "two"
+            , '3' <$ stringRewind "three"
+            , '4' <$ stringRewind "four"
+            , '5' <$ stringRewind "five"
+            , '6' <$ stringRewind "six"
+            , '7' <$ stringRewind "seven"
+            , '8' <$ stringRewind "eight"
+            , '9' <$ stringRewind "nine"
+            ]
+    stringRewind str = lookAhead (string str) *> char (head str)
 
-    parts = [print, print . sum3]
-    sum3 (Input a b c) = a + b + c
+    parts = [print . sum . map firstLast]
+
+    firstLast :: String -> Int
+    firstLast digits = read [head digits, last digits]
